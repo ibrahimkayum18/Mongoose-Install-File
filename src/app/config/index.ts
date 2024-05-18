3f08f898ff23ce6fb52e1514f381504683b0f2c3
ibrahimkayum18 <ibrahimkayum017@gmail.com>
2024-05-18 21:36:07 +0600

    Prettier added

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-import path = require('path')
+import path = require("path");
 
-dotenv.config({path: path.join(( process.cwd(), '.env'))})
+dotenv.config({ path: path.join((process.cwd(), ".env")) });
 
 export default {
-    port: process.env.PORT,
-    database_url: process.env.DATABASE_URL
-}
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+};
